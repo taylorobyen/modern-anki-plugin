f4db3e5f090f6eced0133498d572acbeabfa225c
Taylor Obyen <taylorobyen@gmail.com>
2024-09-21 08:43:23 -0400

    Browser refreshes on card deck change

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
-from aqt import dialogs, gui_hooks
+from anki.collection_pb2 import OpChanges
 from aqt.browser.browser import Browser
 from .browser_utils import add_card
+from aqt import dialogs, gui_hooks
 from typing import cast, Optional
-from anki.notes import Note
 from aqt.qt import qconnect
 
-def refresh_browser(note: Note):
+def handle_operation(changes: OpChanges, initiatior: object):
+    if changes.browser_table:
+        refresh_browser()
+
+def refresh_browser():
     """
     Refresh the browser window by conducting a search with the same
     filter
@@ -17,5 +21,5 @@ def refresh_browser(note: Note):
 def update_add_card_choosers(browser: Browser):
     qconnect(browser.form.actionAdd.triggered, lambda: add_card(browser))
 
-gui_hooks.add_cards_did_add_note.append(refresh_browser)
+gui_hooks.operation_did_execute.append(handle_operation)
 gui_hooks.browser_menus_did_init.append(update_add_card_choosers)
