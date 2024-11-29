262fb5b9536e8003948dcedc8fc87f96ea6d1cd4
Taylor Obyen <162023405+taylorobyen@users.noreply.github.com>
2024-11-29 07:39:20 -0500

    Reimplement refresh on card movement

--- __init__.py
+++ __init__.py
@@ -1,25 +1,45 @@
+"""Hook into the Anki functions."""
+
+from typing import Any, Optional, cast
+
 from anki.collection_pb2 import OpChanges
-from aqt.browser.browser import Browser
-from .browser_utils import add_card
 from aqt import dialogs, gui_hooks
-from typing import cast, Optional
+from aqt.browser.browser import Browser
 from aqt.qt import qconnect
 
-def handle_operation(changes: OpChanges, initiatior: object):
-    if changes.browser_table:
+from .browser_utils import add_card
+
+
+def handle_operation(changes: OpChanges, initiatior: object) -> None:
+    # There isn't a direct hook for card deck changes, so I have to
+    # infer that a deck change occured by checking the attributes
+    # of each operation change and hope it's a deck change.
+    if (
+        changes.browser_table
+        and changes.card
+        and changes.study_queues
+        and changes.mtime
+        and initiatior is None
+    ):
         refresh_browser()
 
-def refresh_browser():
+
+def refresh_browser(*_: list[Any]) -> None:
     """
     Refresh the browser window by conducting a search with the same
-    filter
+    filter.
     """
-    browser = cast(Optional[Browser], dialogs._dialogs["Browser"][1])
+    browser = cast(Optional[Browser], dialogs._dialogs["Browser"][1])  # noqa: SLF001
     if browser:
         browser.search()
 
-def update_add_card_choosers(browser: Browser):
+
+def update_add_card_choosers(browser: Browser) -> None:
     qconnect(browser.form.actionAdd.triggered, lambda: add_card(browser))
 
+
+gui_hooks.add_cards_did_change_deck.append(refresh_browser)
+gui_hooks.add_cards_did_add_note.append(refresh_browser)
 gui_hooks.operation_did_execute.append(handle_operation)
+gui_hooks.browser_menus_did_init.append(update_add_card_choosers)
 gui_hooks.browser_menus_did_init.append(update_add_card_choosers)
--- browser_utils.py
+++ browser_utils.py
@@ -1,33 +1,39 @@
+"""Helper functions for interacting with the browser."""
+
 from __future__ import annotations
 
-from aqt.browser.browser import Browser
-from anki.models import NotetypeId
-from anki.decks import DeckId
-from aqt.addcards import AddCards
-from typing import cast
+from typing import TYPE_CHECKING, cast
+
 import aqt
+from aqt.addcards import AddCards
+
+if TYPE_CHECKING:
+    from anki.decks import DeckId
+    from anki.models import NotetypeId
+    from aqt.browser.browser import Browser
+
 
 def get_active_deck_id(browser: Browser) -> DeckId | None:
-    """
-    Returns the first index of the decks selected on the sidebar
-    """
-    selected_decks = browser.sidebar._selected_decks()
+    """Return the first index of the decks selected on the sidebar."""
+    selected_decks = browser.sidebar._selected_decks()  # noqa: SLF001
     if len(selected_decks) > 0:
         return selected_decks[0]
 
     return None
 
+
 def get_active_note_type_id(browser: Browser) -> NotetypeId | None:
     """
     If multiple cards are selected the note type will be derived
-    from the final card selected
+    from the final card selected.
     """
     if current_note := browser.table.get_current_note():
         return current_note.mid
 
     return None
 
-def add_card(browser: Browser, deck_id: DeckId | None = None):
+
+def add_card(browser: Browser, deck_id: DeckId | None = None) -> None:
     add_cards = cast(AddCards, aqt.dialogs.open("AddCards", browser.mw))
 
     deck_id = deck_id or get_active_deck_id(browser)
